e3f28df76eb1d2ab3c9ff85474fc538b28e803be
sarah-antillia <sarah@antillia.com>
2023-04-09 09:07:48 +0900

    Initial commit

--- projects/BUSI/create_master.py
+++ projects/BUSI/create_master.py
@@ -1,6 +1,20 @@
+# 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #
 # create_master.py
-# 2023/04/10 Antillia.com Toshiyuki Arai
+# 2023/04/10 (C) Antillia.com Toshiyuki Arai
 #
 # This splits the original Dataset_BUSI_with_GT dataset 
 # to three subsets (0.6) train (0.2), test (0.2) and valid. 
