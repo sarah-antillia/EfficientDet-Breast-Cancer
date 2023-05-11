3b267f77b8e3f060f99e593bb04f29f97e82ff80
sarah-antillia <sarah@antillia.com>
2023-05-12 06:57:51 +0900

    Updated requirements.txt

--- projects/BUSI/1_train.bat
+++ projects/BUSI/1_train.bat
@@ -4,7 +4,7 @@ python ../../ModelTrainer.py ^
   --train_file_pattern=./train/*.tfrecord  ^
   --val_file_pattern=./valid/*.tfrecord ^
   --model_name=efficientdet-d0 ^
-  --hparams="autoaugment_policy=v2,learning_rate=0.01,image_size=512x512,num_classes=2,label_map=./label_map.yaml" ^
+  --hparams="learning_rate=0.1,image_size=512x512,num_classes=2,label_map=./label_map.yaml" ^
   --model_dir=./models ^
   --label_map_pbtxt=./label_map.pbtxt ^
   --eval_dir=./eval ^
